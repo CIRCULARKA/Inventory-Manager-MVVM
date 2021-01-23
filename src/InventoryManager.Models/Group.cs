6963e14855d12c881db2cc516dcbae8230436ca1
CIRCULARKA <donkardon@list.ru>
2021-01-23 10:20:58 +0500

    Add model for user groups

---
+++ src/InventoryManager.Models/Group.cs
@@ -0,0 +1,29 @@
+namespace InventoryManager.Models
+{
+	public class Group : NotifyingModel
+	{
+		private int _id;
+
+		private string _name;
+
+		public int ID
+		{
+			get => _id;
+			set
+			{
+				_id = value;
+				base.OnPropertyChanged("ID");
+			}
+		}
+
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				_name = value;
+				base.OnPropertyChanged("Name");
+			}
+		}
+	}
+}
