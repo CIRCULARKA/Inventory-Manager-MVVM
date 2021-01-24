8c3b383183e5d4df28b67f13d84b10038261ec93
CIRCULARKA <donkardon@list.ru>
2021-01-24 11:00:40 +0500

    Add user model

---
+++ src/InventoryManager.Models/User.cs
@@ -0,0 +1,110 @@
+namespace InventoryManager.Models
+{
+	public class User : NotifyingModel
+	{
+		private int _id;
+
+		private string _lastName;
+
+		private string _firstName;
+
+		private string _middleName;
+
+		private string _login;
+
+		private string _password;
+
+		private Group _userGroup;
+
+		public int ID
+		{
+			get
+			{
+				return _id;
+			}
+			set
+			{
+				_id = value;
+				OnPropertyChanged("ID");
+			}
+		}
+
+		public string LastName
+		{
+			get
+			{
+				return _lastName;
+			}
+			set
+			{
+				_lastName = value;
+				OnPropertyChanged("LastName");
+			}
+		}
+
+		public string FirstName
+		{
+			get
+			{
+				return _firstName;
+			}
+			set
+			{
+				_firstName = value;
+				OnPropertyChanged("FirstName");
+			}
+		}
+
+		public string MiddleName
+		{
+			get
+			{
+				return _middleName;
+			}
+			set
+			{
+				_middleName = value;
+				OnPropertyChanged("MiddleName");
+			}
+		}
+
+		public string Login
+		{
+			get
+			{
+				return _login;
+			}
+			set
+			{
+				_login = value;
+				OnPropertyChanged("Login");
+			}
+		}
+
+		public string Password
+		{
+			get
+			{
+				return _password;
+			}
+			set
+			{
+				_password = value;
+				OnPropertyChanged("Password");
+			}
+		}
+
+		public Group UserGroup
+		{
+			get
+			{
+				return _userGroup;
+			}
+			set
+			{
+				_userGroup = value;
+				OnPropertyChanged("UserGroup");
+			}
+		}
+	}
+}
