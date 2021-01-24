ad93050f3ba68d455d4d25f8c751fb8b8570c3d5
CIRCULARKA <donkardon@list.ru>
2021-01-24 14:21:29 +0500

    Add login method prototype

--- src/InventoryManager.Views/AuthorizationView.cs
+++ src/InventoryManager.Views/AuthorizationView.cs
@@ -11,5 +11,11 @@ namespace InventoryManager
 
 			DataContext = new AuthorizationViewModel();
 		}
+
+		public void AttemptToLogin(object sender, RoutedEventArgs info)
+		{
+			// There is must be a call of a ViewModel method that confirms that user exists
+			// If so, method must open user's window and close this view
+		}
 	}
 }
