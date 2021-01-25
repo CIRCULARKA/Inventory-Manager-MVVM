397596997cfb2521dc427ea9501648c4938fef70
CIRCULARKA <donkardon@list.ru>
2021-01-25 11:07:18 +0500

    Add DeviceViewModel

---
+++ src/InventoryManager.ViewModels/DeviceViewModel.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using InventoryManager.Models;
+using InventoryManager.Data;
+using System.Linq;
+
+namespace InventoryManager.ViewModels
+{
+	public class DeviceViewModel
+	{
+		public DeviceViewModel(InventoryManagerDbContext context)
+		{
+			Data = context;
+		}
+
+		public InventoryManagerDbContext Data { get; }
+
+		public IQueryable<Device> Devices => Data.Device;
+
+		public Device SelectedDevice { get; set; }
+
+		public string MessageToUser { get; set; }
+	}
+}
