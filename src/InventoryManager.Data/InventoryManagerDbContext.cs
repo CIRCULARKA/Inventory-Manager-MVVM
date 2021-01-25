33e97fe1fdf30cccac1d5df4347de25b77206c19
CIRCULARKA <donkardon@list.ru>
2021-01-25 10:43:08 +0500

    Add missing entities to Db context

--- src/InventoryManager.Data/InventoryManagerDbContext.cs
+++ src/InventoryManager.Data/InventoryManagerDbContext.cs
@@ -10,6 +10,10 @@ namespace InventoryManager.Data
 
 		public DbSet<User> User { get; set; }
 
+		public DbSet<DeviceType> DeviceType { get; set; }
+
+		public DbSet<Device> Device { get; set; }
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(@"Server=(local);Database=InventoryManagerDb;Trusted_Connection=True");
@@ -20,6 +24,7 @@ namespace InventoryManager.Data
 			builder.ApplyConfiguration<Group>(new GroupModelConfiguration());
 			builder.ApplyConfiguration<User>(new UserModelConfiguration());
 			builder.ApplyConfiguration<DeviceType>(new DeviceTypeModelConfiguration());
+			builder.ApplyConfiguration<Device>(new DeviceModelConfiguration());
 		}
 	}
 }
