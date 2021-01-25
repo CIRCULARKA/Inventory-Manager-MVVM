d6b54ac97fbf73a3e1a47fdadc930d6bc963a25f
CIRCULARKA <donkardon@list.ru>
2021-01-25 10:42:17 +0500

    Add Device configuration

---
+++ src/InventoryManager.Models/Configurations/DeviceModelConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryManager.Models.Configuration
+{
+	public class DeviceModelConfiguration : IEntityTypeConfiguration<Device>
+	{
+		public void Configure(EntityTypeBuilder<Device> builder)
+		{
+			builder.HasKey(d => d.InventoryNumber);
+			builder.Property(d => d.DeviceTypeID).IsRequired();
+			builder.Property(d => d.NetworkName).IsRequired();
+
+			builder.HasData(
+				new Device[]
+				{
+					new Device
+					{
+						InventoryNumber = "NSGK530923",
+						DeviceTypeID = 1,
+						NetworkName = "IVAN-PC"
+					}
+				}
+			);
+		}
+	}
+}
