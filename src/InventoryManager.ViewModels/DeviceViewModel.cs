4aef7778dd3d56437a5d700ec74adfadfb82d141
CIRCULARKA <donkardon@list.ru>
2021-01-27 19:20:27 +0500

    Merge AddDeviceViewModel with DeviceViewModel

--- src/InventoryManager.ViewModels/DeviceViewModel.cs
+++ src/InventoryManager.ViewModels/DeviceViewModel.cs
@@ -1,14 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using InventoryManager.Commands;
 using InventoryManager.Models;
-using InventoryManager.Data;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using InventoryManager.Views;
+using InventoryManager.Extensions;
 using System.Linq;
 
 namespace InventoryManager.ViewModels
 {
 	public class DeviceViewModel : ViewModelBase
 	{
+		private string _messageToUser;
+
+		private string _inputtedInventoryNumber;
+
+		private string _inputtedNetworkName;
+
 		public DeviceViewModel()
 		{
 			OpenAddDeviceViewCommand = new ButtonCommand(
@@ -18,15 +26,75 @@ namespace InventoryManager.ViewModels
 					addDeviceWindow.Show();
 				}
 			);
+
+			AddDeviceCommand = new ButtonCommand(
+				(obj) =>
+				{
+					DataContext.Devices.Add(
+						new Device
+						{
+							InventoryNumber = InputtedInventoryNumber,
+							DeviceType = SelectedDeviceType,
+							NetworkName = InputtedNetworkName
+						}
+					);
+
+					DataContext.SaveChanges();
+
+					InputtedInventoryNumber = "";
+					InputtedNetworkName = "";
+					MessageToUser = "Устройство добавлено";
+				},
+				(obj) =>
+				{
+					return !string.IsNullOrEmpty(InputtedInventoryNumber) &&
+						!string.IsNullOrEmpty(InputtedNetworkName);
+				}
+			);
 		}
+
+		public IEnumerable<DeviceType> DeviceTypes =>
+			DataContext.DeviceTypes.ToList();
+
+		public ButtonCommand AddDeviceCommand { get; }
 
 		public ButtonCommand OpenAddDeviceViewCommand { get; }
 
-		public IQueryable<Device> Devices =>
-			DataContext.Devices.Include(d => d.DeviceType).ToList().AsQueryable();
+		public ObservableCollection<Device> Devices =>
+			DataContext.Devices.Include(d => d.DeviceType).ToList().ToObservableCollection();
 
 		public Device SelectedDevice { get; set; }
 
-		public string MessageToUser { get; set; }
+		public string InputtedInventoryNumber
+		{
+			get => _inputtedInventoryNumber;
+			set
+			{
+				_inputtedInventoryNumber = value;
+				OnPropertyChanged("InputtedInventoryNumber");
+			}
+		}
+
+		public string MessageToUser
+		{
+			get => _messageToUser;
+			set
+			{
+				_messageToUser = value;
+				OnPropertyChanged("MessageToUser");
+			}
+		}
+
+		public DeviceType SelectedDeviceType { get; set; }
+
+		public string InputtedNetworkName
+		{
+			get => _inputtedNetworkName;
+			set
+			{
+				_inputtedNetworkName = value;
+				OnPropertyChanged("InputtedNetworkName");
+			}
+		}
 	}
 }
