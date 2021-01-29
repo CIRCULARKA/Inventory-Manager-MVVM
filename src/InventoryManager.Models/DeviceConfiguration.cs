b4f20127914497b78b372fb27abd75b56dc8a349
CIRCULARKA <donkardon@list.ru>
2021-01-29 12:53:07 +0500

    Add new model - DeviceConfiguration

---
+++ src/InventoryManager.Models/DeviceConfiguration.cs
@@ -0,0 +1,13 @@
+namespace InventoryManager.Models
+{
+	public class DeviceConfiguration
+	{
+		public Device Device { get; set; }
+
+		public int DeviceSerialNumber { get; set; }
+
+		public string AccountName { get; set; }
+
+		public string AccountPassword { get; set; }
+	}
+}
