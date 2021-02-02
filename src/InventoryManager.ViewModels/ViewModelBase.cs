f6b2a273a495aeeff587020e647e8b1fde6b91ec
CIRCULARKA <donkardon@list.ru>
2021-02-02 18:43:12 +0500

    Add MessageToUser property as base property for all view models

--- src/InventoryManager.ViewModels/ViewModelBase.cs
+++ src/InventoryManager.ViewModels/ViewModelBase.cs
@@ -5,11 +5,23 @@ namespace InventoryManager.ViewModels
 {
 	public class ViewModelBase : NotifyingModel
 	{
+		private string _messageToUser;
+
 		protected ViewModelBase()
 		{
 			DataContext = new InventoryManagerDbContext();
 		}
 
 		public InventoryManagerDbContext DataContext { get; }
+
+		public string MessageToUser
+		{
+			get => _messageToUser;
+			set
+			{
+				_messageToUser = value;
+				OnPropertyChanged("MessageToUser");
+			}
+		}
 	}
 }
