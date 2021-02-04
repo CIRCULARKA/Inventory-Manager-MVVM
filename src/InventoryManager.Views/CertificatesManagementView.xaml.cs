793333f15789ee18bb7642ab05a0700695476b57
CIRCULARKA <donkardon@list.ru>
2021-02-04 14:06:41 +0500

    Add code begind file for CertificatesManagementView.xaml

---
+++ src/InventoryManager.Views/CertificatesManagementView.xaml.cs
@@ -0,0 +1,10 @@
+namespace InventoryManager.Views
+{
+	public partial class CertificatesManagementView : ViewBase
+	{
+		public CertificatesManagementView()
+		{
+			InitializeComponent();
+		}
+	}
+}
