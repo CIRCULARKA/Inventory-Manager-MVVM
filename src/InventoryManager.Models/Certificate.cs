7de833449bddfde26557c1d4da266ba955752cc3
CIRCULARKA <donkardon@list.ru>
2021-02-04 13:39:48 +0500

    Init Certificate model

--- src/InventoryManager.Models/Certificate.cs
+++ src/InventoryManager.Models/Certificate.cs
@@ -2,12 +2,7 @@ using System;
 
 namespace InventoryManager.Models
 {
-	public class Certificate : ModelBase<Certificate>
+	public class Certificate
 	{
-		public string Subject { get; set; }
-
-		public DateTime ValidFrom { get; set; }
-
-		public DateTime ValidUntil { get; set; }
 	}
 }
