5ba9ec19d1bf8a53c52df4e2951bbabf83104c76
CIRCULARKA <donkardon@list.ru>
2021-02-04 14:14:22 +0500

    Implement ShowAddCertificateViewCommand

--- src/InventoryManager.ViewModels/CertificateViewModel.cs
+++ src/InventoryManager.ViewModels/CertificateViewModel.cs
@@ -1,7 +1,22 @@
+using InventoryManager.Commands;
+using InventoryManager.Views;
+
 namespace InventoryManager.ViewModels
 {
 	public class CertificateViewModel : ViewModelBase
 	{
-		
+		public CertificateViewModel()
+		{
+			ShowAddCertificateViewCommand = new ButtonCommand(
+				(o) =>
+				{
+					var addCertificateView = new AddCertificateView();
+					addCertificateView.DataContext = this;
+					addCertificateView.ShowDialog();
+				}
+			);
+		}
+
+		public ButtonCommand ShowAddCertificateViewCommand { get; }
 	}
 }
