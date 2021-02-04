d3d71cb10d4f6b19e9d0a3b9a3d95034a5683083
CIRCULARKA <donkardon@list.ru>
2021-02-04 13:50:26 +0500

    Implement certificate model

--- src/InventoryManager.Models/Certificate.cs
+++ src/InventoryManager.Models/Certificate.cs
@@ -1,8 +1,17 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace InventoryManager.Models
 {
-	public class Certificate
+	public class Certificate : ModelBase<Certificate>
 	{
+		public string Subject { get; set; }
+
+		public DateTime ValidFrom { get; set; }
+
+		public DateTime ValidTo { get; set; }
+
+		public override List<Certificate> All() => DataContext.Certificates.ToList();
 	}
 }
