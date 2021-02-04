c42ae7b84aeacc837ded3cd366fb5a493a9787b5
CIRCULARKA <donkardon@list.ru>
2021-02-04 15:14:33 +0500

    Add connection with view to view model

--- src/InventoryManager.ViewModels/CertificateViewModel.cs
+++ src/InventoryManager.ViewModels/CertificateViewModel.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Controls;
 using InventoryManager.Commands;
 using InventoryManager.Views;
 
@@ -5,6 +9,8 @@ namespace InventoryManager.ViewModels
 {
 	public class CertificateViewModel : ViewModelBase
 	{
+		private string _inputtedSubject;
+
 		public CertificateViewModel()
 		{
 			ShowAddCertificateViewCommand = new ButtonCommand(
@@ -15,8 +21,31 @@ namespace InventoryManager.ViewModels
 					addCertificateView.ShowDialog();
 				}
 			);
+
+			AddCertificateCommand = new ButtonCommand(
+				(o) =>
+				{
+					MessageBox.Show(
+						SelectedDates[0] + " " + SelectedDates[SelectedDates.Count - 1]
+					);
+				}
+			);
+		}
+
+		public string InputtedSubject
+		{
+			get => _inputtedSubject;
+			set
+			{
+				_inputtedSubject = value;
+				OnPropertyChanged("InputtedSubject");
+			}
 		}
 
 		public ButtonCommand ShowAddCertificateViewCommand { get; }
+
+		public ButtonCommand AddCertificateCommand { get; }
+
+		public List<DateTime> SelectedDates { get; set; }
 	}
 }
