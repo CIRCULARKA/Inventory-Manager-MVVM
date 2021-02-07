f729e56bea1ae1da15673a3333469e30d8e05767
CIRCULARKA <donkardon@list.ru>
2021-02-07 21:27:40 +0500

    Add all models to ViewModelBase

--- src/InventoryManager.ViewModels/ViewModelBase.cs
+++ src/InventoryManager.ViewModels/ViewModelBase.cs
@@ -1,11 +1,41 @@
 using InventoryManager.Models;
-using InventoryManager.Data;
 
 namespace InventoryManager.ViewModels
 {
 	public class ViewModelBase : NotifyingModel
 	{
 		private string _messageToUser;
+
+		private Device _deviceModel;
+
+		private DeviceType _deviceTypeModel;
+
+		private User _userModel;
+
+		private Group _groupModel;
+
+		private Housing _housingModel;
+
+		private Cabinet _cabinetModel;
+
+		private Certificate _certificateModel;
+
+		private IPAddress _ipAddressModel;
+
+		private Account _accountModel;
+
+		public ViewModelBase()
+		{
+			_deviceModel = new Device();
+			_deviceTypeModel = new DeviceType();
+			_userModel = new User();
+			_groupModel = new Group();
+			_housingModel = new Housing();
+			_cabinetModel = new Cabinet();
+			_certificateModel = new Certificate();
+			_ipAddressModel = new IPAddress();
+			_accountModel = new Account();
+		}
 
 		public string MessageToUser
 		{
@@ -16,5 +46,23 @@ namespace InventoryManager.ViewModels
 				OnPropertyChanged("MessageToUser");
 			}
 		}
+
+		public Device DeviceModel => _deviceModel;
+
+		public Account AccountModel => _accountModel;
+
+		public Cabinet CabinetModel => _cabinetModel;
+
+		public Certificate CertificateModel => _certificateModel;
+
+		public DeviceType DeviceTypeModel => _deviceTypeModel;
+
+		public Group GroupModel => _groupModel;
+
+		public Housing HousingModel => _housingModel;
+
+		public IPAddress IPAddressModel => _ipAddressModel;
+
+		public User UserModel => _userModel;
 	}
 }
