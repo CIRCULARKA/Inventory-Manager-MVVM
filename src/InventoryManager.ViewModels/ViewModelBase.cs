94e90b0da159094dbab90fbc0f13f1505c56eeb4
CIRCULARKA <donkardon@list.ru>
2021-02-19 17:47:37 +0500

    Add Repository requirement for VM's

--- src/InventoryManager.ViewModels/ViewModelBase.cs
+++ src/InventoryManager.ViewModels/ViewModelBase.cs
@@ -4,7 +4,10 @@ namespace InventoryManager.ViewModels
 {
 	public abstract class ViewModelBase : NotifyingModel
 	{
+		protected abstract IRepository Repository { get; }
+
 		private string _messageToUser;
+
 		public string MessageToUser
 		{
 			get => _messageToUser;
