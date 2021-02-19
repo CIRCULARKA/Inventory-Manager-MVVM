42fb301d9a491576b16863f04349c1ce94c1bf7a
CIRCULARKA <donkardon@list.ru>
2021-02-19 16:16:19 +0500

    Add IUserGroup repo

---
+++ src/InventoryManager.Models/Repositories/IUserGroupRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace InventoryManager.Models
+{
+	public interface IUserGroupRepository
+	{
+		void AddUserGroup(UserGroup newGroup);
+
+		void RemoveUserGroup(UserGroup groupToRemove);
+
+		void UpdateUserGroup(UserGroup groupToUpdate);
+
+		IEnumerable<UserGroup> AllUserGroups { get; }
+	}
+}
