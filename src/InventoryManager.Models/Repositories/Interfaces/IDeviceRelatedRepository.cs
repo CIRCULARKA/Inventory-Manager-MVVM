ec72697b01bcf6dc6e82f003d88d905762605377
CIRCULARKA <donkardon@list.ru>
2021-02-19 18:39:55 +0500

    Add two interfaces to IDeviceRelatedRepo

--- src/InventoryManager.Models/Repositories/Interfaces/IDeviceRelatedRepository.cs
+++ src/InventoryManager.Models/Repositories/Interfaces/IDeviceRelatedRepository.cs
@@ -3,7 +3,7 @@ using System.Linq;
 namespace InventoryManager.Models
 {
 	public interface IDeviceRelatedRepository
-		: IDeviceRepository, IDeviceAccountRepository, IIPAddressRepository, IDeviceTypeRepository
+		: IDeviceRepository, IDeviceAccountRepository, IIPAddressRepository, IDeviceTypeRepository, IHousingRepository, ICabinetRepository
 	{
 		IQueryable<DeviceAccount> GetAllDeviceAccounts(Device device) =>
 			DataContext.DeviceAccounts.Where(a => a.DeviceID == device.ID);
