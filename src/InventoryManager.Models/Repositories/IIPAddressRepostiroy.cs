7341377ac0d910e94ca5fb3d5fc3772ef09d6ca3
CIRCULARKA <donkardon@list.ru>
2021-02-19 16:01:59 +0500

    Add IIPAddress repo

---
+++ src/InventoryManager.Models/Repositories/IIPAddressRepostiroy.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace InventoryManager.Models
+{
+	public interface IIPAddressRepository
+	{
+		void AddIPAddress(IPAddress newIP);
+
+		void RemoveIPAddress(IPAddress IPtoRemove);
+
+		void UpdateIPAddress(IPAddress IPtoUpdate);
+
+		IEnumerable<IPAddress> AllIPAddresses { get; }
+	}
+}
