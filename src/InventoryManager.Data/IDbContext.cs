6c49b4f9f1cdf77d56edff74a84d9a39ef1def28
CIRCULARKA <donkardon@list.ru>
2021-02-19 16:23:19 +0500

    Add IDbContext

---
+++ src/InventoryManager.Data/IDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using InventoryManager.Models;
+
+namespace InventoryManager.Data
+{
+	interface IDbContext
+	{
+		DbSet<UserGroup> UserGroups { get; set; }
+
+		DbSet<User> Users { get; set; }
+
+		DbSet<DeviceType> DeviceTypes { get; set; }
+
+		DbSet<Device> Devices { get; set; }
+
+		DbSet<IPAddress> IPAddresses { get; set; }
+
+		DbSet<Certificate> Certificates { get; set; }
+
+		DbSet<Housing> Housings { get; set; }
+
+		DbSet<Cabinet> Cabinets { get; set; }
+
+		DbSet<DeviceAccount> DeviceAccounts { get; set; }
+	}
+}
