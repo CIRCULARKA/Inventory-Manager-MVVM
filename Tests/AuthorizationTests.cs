0f130c67f263595c173a91b329419468605738d3
CIRCULARKA <donkardon@list.ru>
2021-02-19 10:08:16 +0500

    Rename test file

---
+++ Tests/AuthorizationTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace InventoryManager.Tests
+{
+    public class AuthorizationTests
+    {
+        [Fact]
+        public void CanUserBeFound()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
