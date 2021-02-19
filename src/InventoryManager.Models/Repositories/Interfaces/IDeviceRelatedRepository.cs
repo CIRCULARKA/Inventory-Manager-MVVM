755f6f42d2eb3c7c2ed36ea7a6fdff4c44637797
CIRCULARKA <donkardon@list.ru>
2021-02-19 18:45:05 +0500

    Add IDeviceMovementHistory to IDeviceRelatedRepo

--- src/InventoryManager.Models/Repositories/Interfaces/IDeviceRelatedRepository.cs
+++ src/InventoryManager.Models/Repositories/Interfaces/IDeviceRelatedRepository.cs
@@ -3,7 +3,9 @@ using System.Linq;
 namespace InventoryManager.Models
 {
 	public interface IDeviceRelatedRepository
-		: IDeviceRepository, IDeviceAccountRepository, IIPAddressRepository, IDeviceTypeRepository, IHousingRepository, ICabinetRepository
+		: IDeviceRepository, IDeviceAccountRepository,
+			IIPAddressRepository, IDeviceTypeRepository,
+			IHousingRepository, ICabinetRepository, IDeviceMovementHistoryNoteRepository
 	{
 		IQueryable<DeviceAccount> GetAllDeviceAccounts(Device device) =>
 			DataContext.DeviceAccounts.Where(a => a.DeviceID == device.ID);
