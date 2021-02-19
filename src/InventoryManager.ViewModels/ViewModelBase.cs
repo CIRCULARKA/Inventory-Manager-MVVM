94adb4cb7f58fe7db3dca53041fd2d1533248e08
CIRCULARKA <donkardon@list.ru>
2021-02-19 17:37:56 +0500

    Remove repos from ViewModel base

--- src/InventoryManager.ViewModels/ViewModelBase.cs
+++ src/InventoryManager.ViewModels/ViewModelBase.cs
@@ -5,23 +5,6 @@ namespace InventoryManager.ViewModels
 	public abstract class ViewModelBase : NotifyingModel
 	{
 		private string _messageToUser;
-
-		private UserRepository _userRepo = new UserRepository();
-
-		private CertificateRepository _certificateRepo = new CertificateRepository();
-
-		private LocationRepository _locationRepo = new LocationRepository();
-
-		private DeviceRepository _deviceRepo = new DeviceRepository();
-
-		protected UserRepository Users => _userRepo;
-
-		protected DeviceRepository Devices => _deviceRepo;
-
-		protected CertificateRepository Certificates => _certificateRepo;
-
-		protected LocationRepository Locations => _locationRepo;
-
 		public string MessageToUser
 		{
 			get => _messageToUser;
