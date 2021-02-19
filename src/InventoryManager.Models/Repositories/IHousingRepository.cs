411cde0b6072c6ab2d3a3177aa767c1b79e209ea
CIRCULARKA <donkardon@list.ru>
2021-02-19 16:58:01 +0500

    Add IHousingRepo

---
+++ src/InventoryManager.Models/Repositories/IHousingRepository.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManager.Models
+{
+	public interface IHousingRepository : IRepository
+	{
+		void AddHousing(Housing newHousing) =>
+			DataContext.Housings.Add(newHousing);
+
+		void RemoveHousing(Housing housingToRemove) =>
+			DataContext.Housings.Remove(housingToRemove);
+
+		void UpdateHousing(Housing housingToUpdate) =>
+			DataContext.Housings.Remove(housingToUpdate);
+
+		IEnumerable<Housing> AllHousing =>
+			DataContext.Housings.ToList();
+	}
+}
