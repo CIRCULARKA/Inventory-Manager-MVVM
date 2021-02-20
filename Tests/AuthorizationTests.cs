c24b227660c7eef4d26a63262c21ee9d317576fc
CIRCULARKA <donkardon@list.ru>
2021-02-20 10:30:36 +0500

    Add test to check if user can be authorized

--- Tests/AuthorizationTests.cs
+++ Tests/AuthorizationTests.cs
@@ -1,15 +1,39 @@
+using Moq;
 using Xunit;
+using InventoryManager.Models;
+using InventoryManager.ViewModels;
+using InventoryManager.Views;
+using System;
 
 namespace InventoryManager.Tests
 {
     public class AuthorizationTests
     {
         [Fact]
-        public void CanUserBeFound()
+        public void CanUserBeAuthorized()
         {
             // Arrange
+            var userTryingToPass = new User
+            {
+                Login = "Ivan123",
+                Password = "123321"
+            };
+
+            var vm1 = new AuthorizationViewModel(null, null);
+            vm1.AuthorizingUser = userTryingToPass;
+            vm1.InputtedPassword = "123321";
+
+            var vm2 = new AuthorizationViewModel(null, null);
+            vm2.AuthorizingUser = userTryingToPass;
+            vm2.InputtedPassword = "123";
+
             // Act
+            var result1 = vm1.IsUserPasswordCorrect();
+            var result2 = vm2.IsUserPasswordCorrect();
+
             // Assert
+            Assert.True(result1);
+            Assert.False(result2);
         }
     }
 }
