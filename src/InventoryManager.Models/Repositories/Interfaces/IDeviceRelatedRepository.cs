436f34472bb08aea1907a54a8041ce1f94126f5f
CIRCULARKA <donkardon@list.ru>
2021-02-20 09:45:41 +0500

    Update GetAllDeviceHistoryNotes method

--- src/InventoryManager.Models/Repositories/Interfaces/IDeviceRelatedRepository.cs
+++ src/InventoryManager.Models/Repositories/Interfaces/IDeviceRelatedRepository.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManager.Models
 {
@@ -10,8 +12,11 @@ namespace InventoryManager.Models
 		IQueryable<DeviceAccount> GetAllDeviceAccounts(Device device) =>
 			DataContext.DeviceAccounts.Where(a => a.DeviceID == device.ID);
 
-		IQueryable<DeviceMovementHistoryNote> GetAllDeviceHistoryNotes(Device device) =>
-			DataContext.DeviceMovementHistoryNotes.Where(dmn => dmn.DeviceID == device.ID);
+		IEnumerable<DeviceMovementHistoryNote> GetAllDeviceHistoryNotes(Device device) =>
+			DataContext.
+			DeviceMovementHistoryNotes.
+			Include(h => h.TargetCabinet).
+			Where(dmn => dmn.DeviceID == device.ID);
 
 		IQueryable<IPAddress> GetAllDeviceIPAddresses(Device device) =>
 			DataContext.IPAddresses.Where(ip => ip.DeviceID == device.ID);
