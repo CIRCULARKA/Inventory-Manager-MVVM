04fbcc179c813af055f55ae42ecab84ba85c83a4
CIRCULARKA <donkardon@list.ru>
2021-02-21 12:01:38 +0500

    Add new test

--- Tests/CertificatesManagamentTests.cs
+++ Tests/CertificatesManagamentTests.cs
@@ -46,7 +46,7 @@ namespace InventoryManager.Tests
 			moq.Setup(c => c.AllCertificates).Returns(_seedCertificates);
 
 			var vm1 = new CertificateViewModel(moq.Object);
-			var newCertificate = new Certificate { ID = 10, Subject = "Amazon" };
+			var newCertificate = new Certificate { Subject = "Oracle" };
 			vm1.InputtedSubject = newCertificate.Subject;
 
 			// Act
@@ -54,6 +54,25 @@ namespace InventoryManager.Tests
 
 			// Assert
 			Assert.NotEmpty(vm1.CertificatesToShow.Where(c => c.Subject == newCertificate.Subject));
+		}
+
+		[Fact]
+		public void IsRemovedCertificateDoesntShowToUser()
+		{
+			// Arrange
+			var moq = new Mock<ICertificateRelatedRepository>();
+			_seedCertificates = BuildSeedCertificates();
+			moq.Setup(c => c.AllCertificates).Returns(_seedCertificates);
+
+			var vm1 = new CertificateViewModel(moq.Object);
+			var subjectToRemove = "Microsoft";
+			vm1.SelectedCertificate = vm1.CertificatesToShow.Single(c => c.Subject == subjectToRemove);
+
+			// Act
+			vm1.RemoveCertificateCommand.Execute(null);
+
+			// Assert
+			Assert.Empty(vm1.CertificatesToShow.Where(c => c.Subject == subjectToRemove));
 		}
 	}
 }
