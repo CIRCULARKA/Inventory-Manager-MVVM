6658054b8058585bc2d62d8dc5cb3ce150d5759c
CIRCULARKA <donkardon@list.ru>
2021-02-23 09:56:52 +0500

    Add device-adding functional to AddDeviceViewModel

---
+++ src/InventoryManager.ViewModels/AddDeviceViewModel.cs
@@ -0,0 +1,105 @@
+using InventoryManager.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using InventoryManager.Commands;
+
+namespace InventoryManager.ViewModels
+{
+	public class AddDeviceViewModel : ViewModelBase
+	{
+		private string _inputtedInventoryNumber;
+
+		private string _inputtedNetworkName;
+
+		private string _inputtedIPAddress;
+
+		private string _inputtedDeviceAccountName;
+
+		private string _inputtedDevicePassword;
+
+		public AddDeviceViewModel(IDeviceRelatedRepository repo)
+		{
+			Repository = repo;
+
+			AddDeviceCommand = RegisterCommandAction(
+				(obj) =>
+				{
+					var newDevice = new Device
+					{
+						InventoryNumber = InputtedInventoryNumber,
+						DeviceTypeID = SelectedDeviceType.ID,
+						NetworkName = InputtedNetworkName,
+					};
+
+					try
+					{
+						Repository.AddDevice(newDevice);
+						Repository.SaveChanges();
+
+						// Device should be counted as added to storage when added
+						var addedDeviceNote = new DeviceMovementHistoryNote
+						{
+							// N/A cabinet in N/A housing
+							TargetCabinetID = -4,
+							DeviceID = newDevice.ID,
+							Reason = "Доставлено на склад",
+							Date = DateTime.Now
+						};
+						Repository.FixDeviceMovement(addedDeviceNote);
+						Repository.SaveChanges();
+
+						OnDeviceAdded?.Invoke(newDevice);
+
+						InputtedInventoryNumber = "";
+						InputtedNetworkName = "";
+						MessageToUser = "Устройство добавлено";
+					}
+					catch (Exception e)
+					{
+						MessageToUser = e.Message;
+						Repository.RemoveDevice(newDevice);
+					}
+				},
+				(obj) =>
+				{
+					return !string.IsNullOrEmpty(InputtedInventoryNumber) &&
+						!string.IsNullOrEmpty(InputtedNetworkName) &&
+						SelectedDeviceType != null;
+				}
+			);
+		}
+
+		public event Action<Device> OnDeviceAdded;
+
+		private IDeviceRelatedRepository Repository { get; }
+
+		public ButtonCommand AddDeviceCommand { get; }
+
+		public DeviceType SelectedDeviceType { get; set; }
+
+		public List<DeviceType> AllDeviceTypes =>
+			Repository.AllDeviceTypes.ToList();
+
+		public string InputtedInventoryNumber
+		{
+			get => _inputtedInventoryNumber;
+			set
+			{
+				_inputtedInventoryNumber = value;
+				OnPropertyChanged("InputtedInventoryNumber");
+			}
+		}
+
+		public string InputtedNetworkName
+		{
+			get => _inputtedNetworkName;
+			set
+			{
+				_inputtedNetworkName = value;
+				OnPropertyChanged("InputtedNetworkName");
+			}
+		}
+
+	}
+}
