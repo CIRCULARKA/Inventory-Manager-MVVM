376770627096b9c532125aa1d07d332722039d12
CIRCULARKA <donkardon@list.ru>
2021-02-25 10:36:46 +0500

    Add menu and MainVM with command that shows info about author

--- src/InventoryManager.Views/MainViewModel.cs
+++ src/InventoryManager.Views/MainViewModel.cs
@@ -1,7 +1,27 @@
+using InventoryManager.Commands;
+using System.Windows;
+
 namespace InventoryManager.ViewModels
 {
 	public class MainViewModel : ViewModelBase
 	{
+		const string _githubUrl = "https://github.com/CIRCULARKA/Inventory-Manager-MVVM";
 
+		const string _authorName = "Гачегов Руслан. 318 П/1";
+
+		public MainViewModel()
+		{
+			ShowAboutProgramDialogCommand = RegisterCommandAction(
+				(obj) =>
+				{
+					MessageBox.Show(
+						$"Программа для управления инвентарём колледжа.\nАвтор: {_authorName}",
+						"О программе"
+					);
+				}
+			);
+		}
+
+		public Command ShowAboutProgramDialogCommand { get; }
 	}
 }
