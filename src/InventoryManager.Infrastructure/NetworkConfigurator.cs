4d53b388c08fbf36967293800356d09298cc5ab3
CIRCULARKA <donkardon@list.ru>
2021-03-06 11:08:43 +0500

    Finallyvim Implement First and Last hosts

--- src/InventoryManager.Infrastructure/NetworkConfigurator.cs
+++ src/InventoryManager.Infrastructure/NetworkConfigurator.cs
@@ -1,6 +1,8 @@
 using InventoryManager.Models;
 using System.Collections.Generic;
 using System.Text;
+using System;
+using System.Linq;
 
 namespace InventoryManager.Infrastructure
 {
@@ -12,19 +14,47 @@ namespace InventoryManager.Infrastructure
 
 		public string NetworkAddress { get; set; }
 
+		public byte[] NetworkAddressOctets => GetOctetsFromAddress(NetworkAddress);
+
 		public int Mask { get; set; }
 
 		public string FirstHost
 		{
 			get
 			{
-				return null;
+				var result = new byte[_octetsAmount];
+				for (int i = 0; i < _octetsAmount; i++)
+					result[i] = (byte)(NetworkAddressOctets[i] & MaskOctets[i]);
+				if (result[result.Length - 1] < 255) result[result.Length - 1]++;
+
+				return GetAddressFromOctets(result);
 			}
 		}
 
-		public IEnumerable<IPAddress> IPAddresses => null;
+		public string LastHost
+		{
+			get
+			{
+				var result = new byte[_octetsAmount];
+				for (int i = 0; i < _octetsAmount; i++)
+					result[i] = (byte)(NetworkAddressOctets[i] | ~MaskOctets[i]);
+
+				return GetAddressFromOctets(result);
+			}
+		}
+
+		public IEnumerable<InventoryManager.Models.IPAddress> IPAddresses => null;
 
 		public void WriteChanges(INetworkConfigurationWriter writer) { }
+
+		private byte[] MaskOctets
+		{
+			get
+			{
+				uint maskValue = ~(uint.MaxValue >> Mask);
+				return BitConverter.GetBytes(maskValue).Reverse().ToArray();
+			}
+		}
 
 		private byte[] GetOctetsFromAddress(string address)
 		{
