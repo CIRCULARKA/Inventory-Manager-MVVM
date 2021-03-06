457322fbd8b05adfec060a6cc4bc0822d721aed2
CIRCULARKA <donkardon@list.ru>
2021-03-06 11:20:30 +0500

    Improve tests

--- Tests/NetworkConfiguratorTests.cs
+++ Tests/NetworkConfiguratorTests.cs
@@ -1,6 +1,5 @@
 using Xunit;
 using InventoryManager.Infrastructure;
-using System.Linq;
 
 namespace InventoryManager.Tests
 {
@@ -12,11 +11,9 @@ namespace InventoryManager.Tests
 			// Assert
 			var configurator = new NetworkConfigurator();
 
-			var networkAddress = "192.168.33.72";
-
 			// Act
 			configurator.Mask = 26;
-			configurator.NetworkAddress = networkAddress;
+			configurator.NetworkAddress = "192.168.33.72";
 
 			var result1 = configurator.FirstHost;
 
@@ -25,8 +22,20 @@ namespace InventoryManager.Tests
 
 			var result2 = configurator.FirstHost;
 
+			configurator.Mask = 16;
+			configurator.NetworkAddress = "192.168.54.1";
+
+			var result3 = configurator.FirstHost;
+
+			configurator.Mask = 8;
+			configurator.NetworkAddress = "192.168.54.1";
+
+			var result4 = configurator.FirstHost;
+
 			Assert.Equal("192.168.33.65", result1);
 			Assert.Equal("192.168.54.1", result2);
+			Assert.Equal("192.168.0.1", result3);
+			Assert.Equal("192.0.0.1", result4);
 		}
 
 
@@ -49,8 +58,20 @@ namespace InventoryManager.Tests
 
 			var result2 = configurator.LastHost;
 
+			configurator.Mask = 16;
+			configurator.NetworkAddress = "192.168.54.1";
+
+			var result3 = configurator.LastHost;
+
+			configurator.Mask = 8;
+			configurator.NetworkAddress = "192.168.54.1";
+
+			var result4 = configurator.LastHost;
+
 			Assert.Equal("192.168.33.126", result1);
 			Assert.Equal("192.168.54.2", result2);
+			Assert.Equal("192.168.255.254", result3);
+			Assert.Equal("192.255.255.254", result4);
 		}
 	}
 }
