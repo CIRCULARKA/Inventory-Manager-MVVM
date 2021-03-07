6dee7fc3613a6edd3a990f45555307e74332e9ed
CIRCULARKA <donkardon@list.ru>
2021-03-07 17:15:26 +0500

    As everytime, revert all changes

--- src/InventoryManager.Infrastructure/INetworkConfigurationReader.cs
+++ src/InventoryManager.Infrastructure/INetworkConfigurationReader.cs
@@ -4,10 +4,7 @@ namespace InventoryManager.Infrastructure
 {
 	public interface INetworkConfigurationReader
 	{
-		static byte GetMaskFromConfiguration() =>
-			byte.Parse(ConfigurationManager.AppSettings["networkMask"]);
-
-		static string GetNetworkAddressFromConfiguration() =>
-			ConfigurationManager.AppSettings["networkAddress"];
+		byte GetMaskFromConfiguration();
+		string GetNetworkAddressFromConfiguration();
 	}
 }
--- src/InventoryManager.Infrastructure/XMLNetworkConfigurationReader.cs
+++ src/InventoryManager.Infrastructure/XMLNetworkConfigurationReader.cs
@@ -2,5 +2,12 @@ using System.Configuration;
 
 namespace InventoryManager.Infrastructure
 {
-	public class XMLNetworkConfigurationReader : INetworkConfigurationReader { }
+	public class XMLNetworkConfigurationReader : INetworkConfigurationReader
+	{
+		public byte GetMaskFromConfiguration() =>
+			byte.Parse(ConfigurationManager.AppSettings["networkMask"]);
+
+		public string GetNetworkAddressFromConfiguration() =>
+			ConfigurationManager.AppSettings["networkAddress"];
+	}
 }
