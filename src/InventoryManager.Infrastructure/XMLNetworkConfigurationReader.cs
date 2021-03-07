cf2e07f1cbd97574f44ecdb151100aff9968cbe6
CIRCULARKA <donkardon@list.ru>
2021-03-07 17:12:51 +0500

    Remove implementation from child

--- src/InventoryManager.Infrastructure/XMLNetworkConfigurationReader.cs
+++ src/InventoryManager.Infrastructure/XMLNetworkConfigurationReader.cs
@@ -2,12 +2,5 @@ using System.Configuration;
 
 namespace InventoryManager.Infrastructure
 {
-	public class XMLNetworkConfigurationReader : INetworkConfigurationReader
-	{
-		public byte GetMaskFromConfiguration() =>
-			byte.Parse(ConfigurationManager.AppSettings["networkMask"]);
-
-		public string GetNetworkAddressFromConfiguration() =>
-			ConfigurationManager.AppSettings["networkAddress"];
-	}
+	public class XMLNetworkConfigurationReader : INetworkConfigurationReader { }
 }
