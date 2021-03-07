aeb05b9599e0e142914cf8544ba881bcca96127a
CIRCULARKA <donkardon@list.ru>
2021-03-07 17:18:04 +0500

    Add some space

--- src/InventoryManager.Infrastructure/INetworkConfigurationReader.cs
+++ src/InventoryManager.Infrastructure/INetworkConfigurationReader.cs
@@ -5,6 +5,7 @@ namespace InventoryManager.Infrastructure
 	public interface INetworkConfigurationReader
 	{
 		byte GetMaskFromConfiguration();
+
 		string GetNetworkAddressFromConfiguration();
 	}
 }
