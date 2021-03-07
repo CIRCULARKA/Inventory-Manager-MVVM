65b5b92c18ba5e0aff07f557971097d82109bc3c
CIRCULARKA <donkardon@list.ru>
2021-03-07 17:17:50 +0500

    Make XMLNetworkConfigurationReader singletone

--- src/InventoryManager.Infrastructure/XMLNetworkConfigurationReader.cs
+++ src/InventoryManager.Infrastructure/XMLNetworkConfigurationReader.cs
@@ -4,6 +4,18 @@ namespace InventoryManager.Infrastructure
 {
 	public class XMLNetworkConfigurationReader : INetworkConfigurationReader
 	{
+		private static XMLNetworkConfigurationReader _instance;
+
+		public XMLNetworkConfigurationReader Instance
+		{
+			get
+			{
+				if (_instance == null)
+					return new XMLNetworkConfigurationReader();
+				return _instance;
+			}
+		}
+
 		public byte GetMaskFromConfiguration() =>
 			byte.Parse(ConfigurationManager.AppSettings["networkMask"]);
 
