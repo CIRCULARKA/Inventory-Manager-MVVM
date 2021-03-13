4fd3a281e9002f5e736a1abc95f6c10adb40b2f8
CIRCULARKA <donkardon@list.ru>
2021-03-13 19:51:10 +0500

    Add view for searching and filtering

---
+++ src/InventoryManager.Views/DeviceManagement/DeviceSearchAndFileteringView.xaml.cs
@@ -0,0 +1,11 @@
+using InventoryManager.ViewModels;
+using System.Windows.Controls;
+
+namespace InventoryManager.Views
+{
+	public partial class DeviceSearchAndFilteringView : UserControl
+	{
+		public DeviceSearchAndFilteringView() =>
+			InitializeComponent();
+	}
+}
