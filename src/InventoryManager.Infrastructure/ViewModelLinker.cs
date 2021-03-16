ea3f9e58eaa218c6ea92bdbf167f7b4978465389
CIRCULARKA <donkardon@list.ru>
2021-03-16 18:22:21 +0500

    Update ViewModelLinker and make it static

--- src/InventoryManager.Infrastructure/ViewModelLinker.cs
+++ src/InventoryManager.Infrastructure/ViewModelLinker.cs
@@ -1,5 +1,6 @@
 using InventoryManager.ViewModels;
 using InventoryManager.Views;
+using System.Collections.Generic;
 
 namespace InventoryManager.Infrastructure
 {
@@ -8,11 +9,25 @@ namespace InventoryManager.Infrastructure
 	/// </summary>
 	public class ViewModelLinker
 	{
+		private static Dictionary<string, ViewBase> _registeredViews { get; } =
+			new Dictionary<string, ViewBase>();
+
+		private static Dictionary<string, ViewModelBase> _registeredViewModels { get; } =
+			new Dictionary<string, ViewModelBase>();
+
+		public static void RegisterView<T>(T view) where T : ViewBase =>
+			_registeredViews.Add(nameof(view), view);
+
+		public static void RegisterViewModel<T>(T viewModel) where T : ViewModelBase =>
+			_registeredViewModels.Add(nameof(viewModel), viewModel);
+
 		/// <summary>
-		/// You can only link one view model to one view.
-		/// Repeated call of this method with similar view will override previous changes
+		/// You can link only one view model for same view.
+		/// Repeated call of this method with similar view will override previous changes.
+		/// View and view model must be registered with <see href="Register" /> methods before
+		/// being linked
 		/// </summary>
-		public void LinkViewWithViewModel(ViewBase view, ViewModelBase viewModel) =>
-			view.DataContext = viewModel;
+		public static void LinkViewWithViewModel(string viewName, string viewModelName) =>
+			_registeredViews[viewName].DataContext = _registeredViewModels[viewModelName];
 	}
 }
