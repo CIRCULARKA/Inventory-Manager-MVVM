3f3fcbf66f694fc561462481de297528c9168885
CIRCULARKA <donkardon@list.ru>
2021-03-18 17:10:26 +0500

    Override OnClosing event

--- src/InventoryManager.Views/ViewBase.cs
+++ src/InventoryManager.Views/ViewBase.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.ComponentModel;
 using InventoryManager.Data;
 
 namespace InventoryManager.Views
@@ -6,5 +7,11 @@ namespace InventoryManager.Views
 	public class ViewBase : Window
 	{
 		public ViewBase() { }
+
+		protected override void OnClosing(CancelEventArgs info)
+		{
+			info.Cancel = true;
+			this.Hide();
+		}
 	}
 }
