a534178da5498a628302750c95f3301a389e9bcc
CIRCULARKA <donkardon@list.ru>
2021-03-18 16:16:36 +0500

    Load main view content dynamically

--- src/InventoryManager.ViewModels/MainViewModel.cs
+++ src/InventoryManager.ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 using InventoryManager.Commands;
 using InventoryManager.Views;
 using InventoryManager.Models;
+using InventoryManager.Infrastructure;
 using System.Windows;
+using System.Windows.Controls;
 using System.Collections;
 
 namespace InventoryManager.ViewModels
@@ -12,8 +14,12 @@ namespace InventoryManager.ViewModels
 
 		const string _authorName = "Гачегов Руслан. 318 П/1";
 
+		private IEnumerable _mainViewTabs;
+
 		public MainViewModel()
 		{
+			LoadTabItemsContent();
+
 			ShowAboutProgramDialogCommand = RegisterCommandAction(
 				(obj) =>
 				{
@@ -36,7 +42,15 @@ namespace InventoryManager.ViewModels
 			);
 		}
 
-		public IEnumerable MainViewContent { get; set; }
+		public IEnumerable MainViewTabs
+		{
+			get => _mainViewTabs;
+			set
+			{
+				_mainViewTabs = value;
+				OnPropertyChanged(nameof(MainViewTabs));
+			}
+		}
 
 		public ConfigureIPSettingsView NetworkConfigurationView { get; private set; }
 
@@ -45,5 +59,27 @@ namespace InventoryManager.ViewModels
 		public Command ShowSetIPMaskDialogCommand { get; }
 
 		public Command ApplyIPMaskChangesCommand { get; }
+
+		public void LoadTabItemsContent()
+		{
+			MainViewTabs = new TabItem[]
+			{
+				new TabItem()
+				{
+					Header = "Устройства",
+					Content = ViewModelLinker.GetRegisteredPartialView(nameof(DevicesManagementView))
+				},
+				new TabItem()
+				{
+					Header = "Пользователи",
+					Content = ViewModelLinker.GetRegisteredPartialView(nameof(DevicesManagementView))
+				},
+				new TabItem()
+				{
+					Header = "Сертификаты",
+					Content = ViewModelLinker.GetRegisteredPartialView(nameof(DevicesManagementView))
+				}
+			};
+		}
 	}
 }
