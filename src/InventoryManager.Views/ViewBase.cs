dd5375c21138f3bed173e159c6b3d49f40c31b36
CIRCULARKA <donkardon@list.ru>
2021-03-19 10:13:26 +0500

    Actually dispose application on MainView closing

--- src/InventoryManager.Views/ViewBase.cs
+++ src/InventoryManager.Views/ViewBase.cs
@@ -1,10 +1,16 @@
 using System.Windows;
+using System.ComponentModel;
 
 namespace InventoryManager.Views
 {
 	public class ViewBase : Window
 	{
 		public ViewBase() { }
+
+		protected override void OnClosing(CancelEventArgs info)
+		{
+			Application.Current.Shutdown();
+		}
 
 	}
 }
