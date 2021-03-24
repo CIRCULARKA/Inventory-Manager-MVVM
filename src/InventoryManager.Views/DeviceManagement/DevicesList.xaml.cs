5bfd1d4916c5919f9861114327c340b0b5f00c17
CIRCULARKA <donkardon@list.ru>
2021-03-24 10:00:58 +0500

    Move devices list into separate view

---
+++ src/InventoryManager.Views/DeviceManagement/DevicesList.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows.Controls;
+
+namespace InventoryManager.Views
+{
+	public partial class DevicesList : UserControl
+	{
+		public DevicesList() =>
+			InitializeComponent();
+	}
+}
