408e958233abdb6320f4f24ca3fa7933933f3e85
CIRCULARKA <donkardon@list.ru>
2021-03-29 17:16:11 +0500

    Move button of device history into a separate view

---
+++ src/InventoryManager.Views/DeviceManagement/DeviceHistoryView.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows.Controls;
+
+namespace InventoryManager.Views
+{
+	public class DeviceHistoryView : UserControl
+	{
+		public DeviceHistoryView() =>
+			InitializeComponent();
+	}
+}
