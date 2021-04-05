0ece239c38c8635be9a3030de98e9a46b6222fb5
CIRCULARKA <donkardon@list.ru>
2021-04-05 11:05:19 +0500

    Add IsAllowedTo method to define is user can do specified thing

--- src/InventoryManager.Infrastructure/AuthorizedUser.cs
+++ src/InventoryManager.Infrastructure/AuthorizedUser.cs
@@ -4,9 +4,22 @@ namespace InventoryManager.Infrastructure
 {
 	public class AuthorizedUser
 	{
+		private UserAccessRules _rules;
+
+		private User _authorizedUser;
+
+		public AuthorizedUser(User user, UserAccessRules rules)
+		{
+			_authorizedUser = user;
+			_rules = rules;
+		}
+
 		public User User { get; set; }
 
 		public UserAccessRights AccessLevel =>
 			(UserAccessRights)User.UserGroupID;
+
+		public bool IsAllowedTo(UserActions action) =>
+			_rules.IsActionAllowed(action);
 	}
 }
