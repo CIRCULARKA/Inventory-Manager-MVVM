330a5c798284cb00b60dc6b3f71eac99efa11839
CIRCULARKA <donkardon@list.ru>
2021-04-19 11:53:59 +0500

    Return properties

--- src/InventoryManager.ViewModels/DeviceManagementViewModel.cs
+++ src/InventoryManager.ViewModels/DeviceManagementViewModel.cs
@@ -7,43 +7,62 @@ namespace InventoryManager.ViewModels
 {
 	public class DevicesManagementViewModel : ViewModelBase
 	{
+		DevicesListView _devicesListPartialView = new DevicesListView();
+
+		DeviceIPListView _deviceIPListPartialView = new DeviceIPListView();
+
+		DeviceAccountsListView _deviceAccountsListPartialView = new DeviceAccountsListView();
+
+		DeviceLocationView _deviceLocationPartialView = new DeviceLocationView();
+
+		DeviceSearchAndFilteringView _deviceSearchAndFilteringPartialView = new DeviceSearchAndFilteringView();
+
+		DeviceHistoryView _deviceHistoryPartialView = new DeviceHistoryView();
+
 		public DevicesManagementViewModel()
 		{
 			var _devicesListViewModel = new DevicesListViewModel(
 				StandartNinjectKernel.Get<IDeviceRelatedRepository>()
 			);
-			var _devicesListPartialView = new DevicesListView();
 			_devicesListPartialView.DataContext = _devicesListViewModel;
 
 			var _deviceIPListViewModel = new DeviceIPListViewModel(
 				StandartNinjectKernel.Get<IDeviceRelatedRepository>()
 			);
-			var _deviceIPListPartialView = new DeviceIPListView();
 				_deviceIPListPartialView.DataContext = _deviceIPListPartialView;
 
 			var _deviceAccountsListViewModel = new DeviceAccountsListViewModel(
 				StandartNinjectKernel.Get<IDeviceRelatedRepository>()
 			);
-			var _deviceAccountsListPartialView = new DeviceAccountsListView();
 			_deviceAccountsListPartialView.DataContext = _deviceAccountsListViewModel;
 
 			var _deviceLocationViewModel = new DeviceLocationViewModel(
 				StandartNinjectKernel.Get<IDeviceRelatedRepository>()
 			);
-			var _deviceLocationPartialView = new DeviceLocationView();
 			_deviceLocationPartialView.DataContext = _deviceLocationViewModel;
 
 			var _deviceSearchAndFilteringViewModel = new DeviceSearchAndFilteringViewModel(
 				new DeviceFilter()
 			);
-			var _deviceSearchAndFilteringPartialView = new DeviceSearchAndFilteringView();
 			_deviceSearchAndFilteringPartialView.DataContext = _deviceSearchAndFilteringViewModel;
 
 			var _deviceHistoryViewModel = new DeviceHistoryViewModel(
 				StandartNinjectKernel.Get<IDeviceRelatedRepository>()
 			);
-			var _deviceHistoryPartialView = new DeviceHistoryView();
 			_deviceHistoryPartialView.DataContext = _deviceHistoryViewModel;
 		}
+
+		public DevicesListView DevicesListPartialView => _devicesListPartialView;
+
+		public DeviceIPListView DeviceIPListPartialView => _deviceIPListPartialView;
+
+		public DeviceAccountsListView DeviceAccountsListPartialView => _deviceAccountsListPartialView;
+
+		public DeviceLocationView DeviceLocationPartialView => _deviceLocationPartialView;
+
+		public DeviceSearchAndFilteringView DeviceSearchAndFilteringPartialView =>
+			_deviceSearchAndFilteringPartialView;
+
+		public DeviceHistoryView DeviceHistoryPartialView => _deviceHistoryPartialView;
 	}
 }
