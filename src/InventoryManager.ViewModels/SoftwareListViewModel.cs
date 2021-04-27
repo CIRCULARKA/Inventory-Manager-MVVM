2f0b3bef357ba4f4ede7ffffcc68f3b30818ce93
CIRCULARKA <donkardon@list.ru>
2021-04-27 19:11:50 +0500

    Implement commands

--- src/InventoryManager.ViewModels/SoftwareListViewModel.cs
+++ src/InventoryManager.ViewModels/SoftwareListViewModel.cs
@@ -1,4 +1,7 @@
 using InventoryManager.Models;
+using InventoryManager.Commands;
+using System.Collections.Generic;
+using System.Windows;
 
 namespace InventoryManager.ViewModels
 {
@@ -7,8 +10,35 @@ namespace InventoryManager.ViewModels
 		public SoftwareListViewModel(IDeviceRelatedRepository repo)
 		{
 			Repository = repo;
+
+			ShowAddSoftwareViewCommand = RegisterCommandAction(
+				(obj) => MessageBox.Show(
+					"Not yet implemented",
+					"Will be implemented soon",
+					MessageBoxButton.OK,
+					MessageBoxImage.Exclamation
+				),
+				(obj) => SelectedSoftware != null
+			);
+
+			RemoveSoftwareCommand = RegisterCommandAction(
+				(obj) => Repository.RemoveSoftware(SelectedSoftware),
+				(obj) => SelectedSoftware != null
+			);
 		}
 
 		private IDeviceRelatedRepository Repository { get; }
+
+		public IEnumerable<Software> SelectedDeviceSoftware =>
+			Repository.GetAllDeviceSoftware(
+				(ResolveDependency<IDevicesListViewModel>() as DevicesListViewModel).
+					SelectedDevice
+			);
+
+		public Software SelectedSoftware { get; set; }
+
+		public Command ShowAddSoftwareViewCommand { get; }
+
+		public Command RemoveSoftwareCommand { get; }
 	}
 }
