0afe7f810369f11d253f89956043b5ce744c30e3
CIRCULARKA <donkardon@list.ru>
2021-04-27 19:01:47 +0500

    Inject repository into new SoftwareListVM

--- src/InventoryManager.ViewModels/SoftwareListViewModel.cs
+++ src/InventoryManager.ViewModels/SoftwareListViewModel.cs
@@ -1,7 +1,14 @@
+using InventoryManager.Models;
+
 namespace InventoryManager.ViewModels
 {
 	public class SoftwareListViewModel : ViewModelBase, ISoftwareListViewModel
 	{
+		public SoftwareListViewModel(IDeviceRelatedRepository repo)
+		{
+			Repository = repo;
+		}
 
+		private IDeviceRelatedRepository Repository { get; }
 	}
 }
