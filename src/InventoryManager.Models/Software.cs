a87e920de4c8ab795c99f096543b6015a4bc0444
CIRCULARKA <donkardon@list.ru>
2021-05-06 11:22:17 +0500

    Change model

--- src/InventoryManager.Models/Software.cs
+++ src/InventoryManager.Models/Software.cs
@@ -6,8 +6,14 @@ namespace InventoryManager.Models
 
 		public int DeviceID { get; set; }
 
-		public int SoftwareTypeID { get; set; }
+		public Device Device { get; set; }
+
+		public int TypeID { get; set; }
 
 		public SoftwareType Type { get; set; }
+
+		public int ConfigurationID { get; set; }
+
+		public SoftwareConfiguration Configuration { get; set; }
 	}
 }
